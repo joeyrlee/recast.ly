c2ebe4b0946e7bed7e16cc2fb3b48cf9fb5f5f74
Hack Reactor Students <accounts@hackreactor.com>
2017-01-06 20:28:13 -0800

    Passed tests through to and including VideoListEntrySpec

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,40 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer/>
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={exampleVideoData}/>
+      </div>
+    </div>);
+  }
+
+}
+
+window.App = App;
+
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList/>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
+
+// document.body.append('this works');  
+
+
+
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,3 +9,4 @@ var Nav = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Nav = Nav;
+
