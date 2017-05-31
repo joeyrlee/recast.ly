0910866ad7fb60505ad7f7785bce8947e58e47e8
Joey Lee <jleepianist@gmail.com>
2017-05-30 22:30:35 -0700

    (feat-update) Integrated GET for YouTube API videos

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({query, max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+  .done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  })
+  .fail(({responseJSON}) => {
+    responseJSON.error.errors.forEach((err) =>
+      console.error(err)
+    );
+  });
 };
+
 
 window.searchYouTube = searchYouTube;
