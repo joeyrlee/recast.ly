8f2a09a2aeb409010cdb71b32a84a2501eea091e
Joey Lee <jleepianist@gmail.com>
2017-05-30 22:12:01 -0700

    (feat-update) Added video click event handler fns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,66 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      videos: [],
+      currentVideo: null
+    };
+  }
+
+  componentDidMount() {
+    this.getYouTubeVideos('react tutorials');
+  }
+
+  getYouTubeVideos(query) {
+    var options = {
+      query: query
+    };
+
+    this.props.searchYouTube(options, (videos) =>
+      this.setState({
+        videos: videos,
+        currentVideo: videos[0]
+      })
+    );
+  }
+
+  handleVideoListEntryTitleClick(video) {
+    this.setState({
+      currentVideo: video
+    });
   }
 
   render() {
-    return (<div>
-      <Nav />
-      <div className="col-md-7">
-        <VideoPlayer/>
+    return (
+      <div>
+        <Nav
+          handleSearchInputChange={this.getYouTubeVideos.bind(this)}
+        />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+        {/*
+          * It's very important to bind the context of this callback.
+          * Also acceptable is to pass a anonymous functoin expression with a fat
+          * arrow that inherits the surrounding lexical `this` context:
+          *
+          *   handleVideoListEntryTitleClick={(video) => this.onVideoListEntryClick(video)}
+          *                                  - or -
+          *   handleVideoListEntryTitleClick={(currentVideo) => this.setState({currentVideo})}
+          *
+          */}
+          <VideoList
+            handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
+            videos={this.state.videos}
+          />
+        </div>
       </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>);
+    );
   }
-
 }
-
-window.App = App;
-
-// var App = () => (
-//   <div>
-//     <Nav />
-//     <div className="col-md-7">
-//       <VideoPlayer/>
-//     </div>
-//     <div className="col-md-5">
-//       <VideoList/>
-//     </div>
-//   </div>
-// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-
-// document.body.append('this works');  
-
-
-
+window.App = App;
